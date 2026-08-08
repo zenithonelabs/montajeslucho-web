5938a713a727b9e3e603e1a27e258da77d82612d
javierhersan <javier.hernandezsanchez@outlook.com>
2026-08-08 21:15:05 +0200

    feat: add locals for github oidc

---
+++ infrastructure/locals.tf
@@ -0,0 +1,5 @@
+locals {
+    github_oidc_url = "https://token.actions.githubusercontent.com"
+    github_oidc_client_id = "sts.amazonaws.com"
+    github_oidc_thumbprint = "6938fd4d98bab03faadb97b34396831e3780aea1"
+}
--- infrastructure/pipeline.tf
+++ infrastructure/pipeline.tf
@@ -1,34 +1,33 @@
 # OIDC provider for GitHub Actions
 resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's thumbprint
+  url = local.github_oidc_url
+  client_id_list = [local.github_oidc_client_id]
+  thumbprint_list = [local.github_oidc_thumbprint] 
 }
 
-# IAM role for CI/CD pipelines
+# IAM role assumed by GitHub Action workflow.
 resource "aws_iam_role" "pipeline_oidc" {
   name = "pipeline-oidc-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-        {
+      {
         Effect = "Allow"
         Principal = {
-            Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
-            StringLike = {
+          StringLike = {
             "token.actions.githubusercontent.com:sub" = [
               "repo:${var.github_repository}:ref:refs/heads/${var.github_repository_branch}",
               "repo:${var.github_repository}:environment:production",
               "repo:${var.github_repository_with_id}:ref:refs/heads/${var.github_repository_branch}",
               "repo:${var.github_repository_with_id}:environment:production"
             ]
-            }
+          }
         }
-        }
+      }
     ]
   })
 }
